0d35f16278fbd7db11413ee351bbc7f4d0ae96cb
seraraja <seraraja555@gmail.com>
2022-08-18 09:34:05 +0530

    New

---
+++ src/test/java/org/test/Employee.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Employee {
+
+	private void empId() {
+		System.out.println("Employee id is 768");
+
+	}
+
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		e.empId();
+		
+	}
+	
+}
